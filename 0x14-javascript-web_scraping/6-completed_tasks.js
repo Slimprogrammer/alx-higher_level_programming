ac0f1e7df889ca14cd7ebc8640e4187f63ce3f7c
Slimprogrammer <slimdanney15@gmail.com>
2023-10-25 00:07:07 +0300

    0x14-javascript-web_scraping Task 4 and 6 Fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,22 @@
 #!/usr/bin/node
-// A script that computes the number of tasks completed by user id.
 
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
   if (error) {
-    console.log('error:', error);
-  } else {
-    let todos = JSON.parse(body);
-    let dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      let status = (todos[i]['completed']);
-      let key = todos[i]['userId'].toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
-        } else {
-          dash[key] = 1;
-        }
+    console.error(error);
+  }
+  const dict = JSON.parse(body).reduce((acc, elem) => {
+    if (!acc[elem.userId]) {
+      if (elem.completed) {
+        acc[elem.userId] = 1;
+      }
+    } else {
+      if (elem.completed) {
+        acc[elem.userId] += 1;
       }
     }
-    console.log(dash);
-  }
+    return acc;
+  }, {});
+  console.log(dict);
 });
